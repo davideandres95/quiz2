97c129ac7bfd30ac7d365dbfce87e8cb584fe2c7
davideandres95 <davideandres95@gmail.com>
2016-06-03 16:50:58 +0200

    fixes

--- controllers/comment_Controller.js
+++ controllers/comment_Controller.js
@@ -20,31 +20,31 @@ exports.new = function (req, res, next) {
     var comment = models.Comment.build({text: ""});
 
     res.render('comments/new',  { comment: comment,
-                                  quiz: req.quiz
-                                });
+        quiz: req.quiz
+    });
 };
 
 //POST /quizzes/:quizId/comments
 exports.create = function (req, res, next) {
     var comment = models.Comment.build(
         { text: req.body.comment.text,
-          QuizId: req.quiz.id
+            QuizId: req.quiz.id
         });
 
     comment.save()
         .then(function (comment) {
-        req.flash('success', 'Comentario creado con exito');
-        res.redirect('/quizzes/'+ req.quiz.id);
-    })
-    .catch(Sequilize.ValidationError, function(error) {
+            req.flash('success', 'Comentario creado con exito');
+            res.redirect('/quizzes/'+ req.quiz.id);
+        })
+        .catch(Sequilize.ValidationError, function(error) {
 
-        req.flash('error', 'Errores en el formulario:');
-        for (var i in errors.errors) {
-            req.flash('error', error.errors[i].value);
-        };
-        res.render('comments/new', {comment: comment,
-                                    quiz:    req.quiz});
-    })
+            req.flash('error', 'Errores en el formulario:');
+            for (var i in errors.errors) {
+                req.flash('error', error.errors[i].value);
+            };
+            res.render('comments/new', {comment: comment,
+                quiz:    req.quiz});
+        })
         .catch(function(error) {
             req.flash('error', 'Error al crear un Comentario: ' +error.message);
             next(error);
--- models/index.js
+++ models/index.js
@@ -22,4 +22,17 @@ var sequelize = new Sequelize (url,
 //Importar la definicion de la tabla Quiz de quiz.js
 var Quiz = sequelize.import(path.join(__dirname, 'quiz'));
 
+//Importar la definición de la tabla Comments de comment.js
+var Comment = sequelize.import(path.join(__dirname, 'comment'));
+
+//Importar la definición de la tabla User de user.js
+var User = sequelize.import(path.join(__dirname, 'user'));
+
+
+//Relaciones entre modelos
+Comment.belongsTo(Quiz);
+Quiz.hasMany(Comment);
+
 exports.Quiz = Quiz; 	// exportar definición de tabla quiz
+exports.Comment = Comment; //exportar definición de tabla comment
+exports.User = User;
